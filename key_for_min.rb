e044fb242ad78658547a895096b471de39f3b453
Samara Grant <samara.amanda01@gmail.com>
2020-07-18 02:24:30 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,14 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
+
+def key_for_min_value(shopping)
+  shopping_list = {
+    :shirts => 2,
+    :pants => 3,
+    :sweaters => 5
+  }
+
+  shopping_list[:shirts]
 
 end
